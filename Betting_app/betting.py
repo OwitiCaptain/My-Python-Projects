0a6ee8a7625d2a18df2f18d50f7987db44bfce9c
KENNEDY ONYANGO <konyango98@gmail.com>
2025-10-17 08:41:18 +0300

    Update betting.py

--- Betting_app/betting.py
+++ Betting_app/betting.py
@@ -22,74 +22,82 @@ def chose_bet():
         print(f"3. {bet5} vs {bet6}")
         print(f"4. {bet4} vs {bet2}")
     elif answer.lower() == "no" or answer.lower() == "n":
-        sys.exit()
+        sys.exit("Goodbye 👋👋")
     else:
         print("Invalid selection!")
         chose_bet()
+
 chose_bet()
+while True:
+    my_selection = input("\nChoose a team between 1-4: ")
 
-
-my_selection = input("\nChoose a team between 1-4: ")
-
-if my_selection == "1":
-    my_bet = input("Which team do you predict to win?: ")
-    print(f"\n{bet1} vs {bet2} is playing. Wait for 5 seconds ........")
-    time.sleep(5)
-    print("Please wait.......")
-    time.sleep(3)
-    print("Please wait.......")
-    time.sleep(2)
-    winner = random.choice([bet1, bet2])
-    if my_bet.upper() == winner.upper():
-        print(f"Congratulations, {my_bet} WON 🎉🎊🎉🎊")
+    if my_selection == "1":
+        my_bet = input("Which team do you predict to win in the selection?: ")
+        print(f"\n{bet1} vs {bet2} is playing. Wait for 5 seconds ........")
+        time.sleep(5)
+        print("Please wait.......")
+        time.sleep(3)
+        print("Please wait.......")
+        time.sleep(2)
+        winner = random.choice([bet1, bet2])
+        if my_bet.upper() == winner.upper():
+            print(f"Congratulations, {my_bet} WON 🎉🎊🎉🎊")
+            break
+        else:
+            print("SORRY, you Lost!")
+            print(f"{winner} Won")
+            continue
+    elif my_selection == "2":
+        my_bet = input("Which team do you predict to win?: ")
+        print(f"\n{bet3} vs {bet4} is playing. Wait Wait for 5 seconds ........")
+        time.sleep(5)
+        print("Please wait.......")
+        time.sleep(3)
+        print("Please wait.......")
+        time.sleep(2)
+        winner = random.choice([bet3,bet4])
+        if my_bet.upper() == winner.upper():
+            print(f"Congratulations, {my_bet} WON 🎉🎊🎉🎊")
+            break
+        else:
+            print("SORRY, you Lost!")
+            print(f"{winner} Won")
+            continue
+    elif my_selection == "3":
+        my_bet = input("Which team do you predict to win?: ")
+        print(f"\n{bet5} vs {bet6} is playing. Wait Wait for 5 seconds ........")
+        time.sleep(5)
+        print("Please wait.......")
+        time.sleep(3)
+        print("Please wait.......")
+        time.sleep(2)
+        winner = random.choice([bet5,bet6])
+        if my_bet.upper() == winner.upper():
+            print(f"Congratulations, {my_bet} WON 🎉🎊🎉🎊")
+            break
+        else:
+            print("SORRY, you Lost!")
+            print(f"{winner} Won")
+            continue
+    elif my_selection == "4":
+        my_bet = input("Which team do you predict to win?: ")
+        print(f"\n{bet4} vs {bet2} is playing. Wait Wait for 5 seconds ........")
+        time.sleep(5)
+        print("Please wait.......")
+        time.sleep(3)
+        print("Please wait.......")
+        time.sleep(2)
+        winner = random.choice([bet4,bet2])
+        if my_bet.upper() == winner.upper():
+            print(f"Congratulations, {my_bet} WON 🎉🎊🎉🎊")
+            break
+        else:
+            print("SORRY, you Lost!")
+            print(f"{winner} Won")
+            continue
     else:
-        print("SORRY, you Lost!")
-        print(f"{winner} Won")
-elif my_selection == "2":
-    my_bet = input("Which team do you predict to win?: ")
-    print(f"\n{bet3} vs {bet4} is playing. Wait Wait for 5 seconds ........")
-    time.sleep(5)
-    print("Please wait.......")
-    time.sleep(3)
-    print("Please wait.......")
-    time.sleep(2)
-    winner = random.choice([bet3,bet4])
-    if my_bet.upper() == winner.upper():
-        print(f"Congratulations, {my_bet} WON 🎉🎊🎉🎊")
-    else:
-        print("SORRY, you Lost!")
-        print(f"{winner} Won")
-elif my_selection == "3":
-    my_bet = input("Which team do you predict to win?: ")
-    print(f"\n{bet5} vs {bet6} is playing. Wait Wait for 5 seconds ........")
-    time.sleep(5)
-    print("Please wait.......")
-    time.sleep(3)
-    print("Please wait.......")
-    time.sleep(2)
-    winner = random.choice([bet5,bet6])
-    if my_bet.upper() == winner.upper():
-        print(f"Congratulations, {my_bet} WON 🎉🎊🎉🎊")
-    else:
-        print("SORRY, you Lost!")
-        print(f"{winner} Won")
-elif my_selection == "4":
-    my_bet = input("Which team do you predict to win?: ")
-    print(f"\n{bet4} vs {bet2} is playing. Wait Wait for 5 seconds ........")
-    time.sleep(5)
-    print("Please wait.......")
-    time.sleep(3)
-    print("Please wait.......")
-    time.sleep(2)
-    winner = random.choice([bet4,bet2])
-    if my_bet.upper() == winner.upper():
-        print(f"Congratulations, {my_bet} WON 🎉🎊🎉🎊")
-    else:
-        print("SORRY, you Lost!")
-        print(f"{winner} Won")
-else:
-    print("Invalid team!")
-    sys.exit("Bye!")
+        print("Invalid team!")
+        continue
 
 
 
